812e35ba2885503e74e328b6d507717abbf19ad5
dotnetCarpenter <jon.ronnenberg@gmail.com>
2020-12-04 01:14:10 +0100

    eq

--- toys.mjs
+++ toys.mjs
@@ -5,12 +5,13 @@ import quote from './lib/quote.mjs'
 import car from './lib/car.mjs'
 import cdr from './lib/cdr.mjs'
 import cons from './lib/cons.mjs'
+import eq from './lib/eq.mjs'
 
 
 assert(isAtom([]) === false, "No because [] is just a list.")
 
 assert(car(['a','b','c']) === 'a', "a because a is the first atom of this list.")
-assert(equals( car([['a', 'b', 'c'], 'x', 'y', 'z']), ['a', 'b', 'c'] ), "because [a b c] is the first S-expression of this non-empty list.")
+assert(eq( car([['a', 'b', 'c'], 'x', 'y', 'z']), ['a', 'b', 'c'] ), "because [a b c] is the first S-expression of this non-empty list.")
 assert(car("hotdog") === null, "You cannot ask for the car of an atom.")
 assert(car([]) === null, "You cannot ask for the car of the empty list.")
 
@@ -18,13 +19,13 @@ assert(car([]) === null, "You cannot ask for the car of the empty list.")
 // The primitive car is defined
 // only for non-empty lists.
 
-assert(equals( car([[['hotdogs']], ['and'], ['pickle'], 'relish']), [['hotdogs']]), "The list of the list of hotdogs. [['hotdogs']] is the first S-expression.")
-assert(equals(car(car( [[['hotdogs']], ['and']] )), ['hotdogs']), "['hotdogs']")
+assert(eq( car([[['hotdogs']], ['and'], ['pickle'], 'relish']), [['hotdogs']]), "The list of the list of hotdogs. [['hotdogs']] is the first S-expression.")
+assert(eq(car(car( [[['hotdogs']], ['and']] )), ['hotdogs']), "['hotdogs']")
 
 
-assert(equals(cdr(['a', 'b', 'c']), ['b', 'c']), "because ['b', 'c'] is the list ['a', 'b', 'c'] without ( car(['a', 'b', 'c'])).")
-assert(equals(cdr([['a', 'b', 'c'], 'x', 'y', 'z']), ['x', 'y', 'z']), "['x', 'y', 'z']")
-assert(equals(cdr(['hamburger']), []), "[]")
+assert(eq(cdr(['a', 'b', 'c']), ['b', 'c']), "because ['b', 'c'] is the list ['a', 'b', 'c'] without ( car(['a', 'b', 'c'])).")
+assert(eq(cdr([['a', 'b', 'c'], 'x', 'y', 'z']), ['x', 'y', 'z']), "['x', 'y', 'z']")
+assert(eq(cdr(['hamburger']), []), "[]")
 assert(cdr('hotdogs') === null, "You cannot ask for the cdr of an atom.")
 assert(cdr([]) === null, "You cannot ask for the cdr of the null list.")
 
@@ -32,16 +33,16 @@ assert(cdr([]) === null, "You cannot ask for the cdr of the null list.")
 // The primitive cdr is defined only for
 // non-empty lists. The cdr of any nonempty list is always another list.
 
-assert(equals(car(cdr( [['b'], ['x', 'y'], [['c']]] )), ['x', 'y']), "(x y), because ((x y) ((c))) is (cdr l), and (x y) is the car of (cdr l).")
-assert(equals(cdr(cdr( [['b'], ['x', 'y'], [['c']]] )), [[['c']]] ), "(((c))), because ((x y) ((c))) is (cdr l), and (((c))) is  the cdr of (cdr l).")
+assert(eq(car(cdr( [['b'], ['x', 'y'], [['c']]] )), ['x', 'y']), "(x y), because ((x y) ((c))) is (cdr l), and (x y) is the car of (cdr l).")
+assert(eq(cdr(cdr( [['b'], ['x', 'y'], [['c']]] )), [[['c']]] ), "(((c))), because ((x y) ((c))) is (cdr l), and (((c))) is  the cdr of (cdr l).")
 assert(cdr(car( ['a', ['b', ['c']], 'd'] )) === null, "No answer, since (car l) is an atom,  and cdr does not take an  atom as an  argument; see The Law of Cdr.")
 
 
-assert(equals( cons('peanut', ['butter', 'jelly']) , [ 'peanut', 'butter', 'jelly' ]), "(peanut butter and jelly), because cons adds  an  atom to  the front of a list.")
-assert(equals( cons(['banana', 'and'], [ 'peanut', 'butter', 'jelly' ]) , [ [ 'banana', 'and' ], 'peanut', 'butter', 'jelly' ]), "((banana and) peanut butter and jelly), because cons adds any S-expression to the front of a list.")
-assert(equals( cons([['help'], 'this'], ['is', 'very', [['very'], 'to', 'learn']]) , [ [ [ 'help' ], 'this' ], 'is', 'very', [ [ 'very' ], 'to', 'learn' ] ]), "(((help) this) is  very  ((hard) to learn)).")
-assert(equals( cons(['a', 'b', ['c']], []), [ [ 'a', 'b', [ 'c' ] ] ]) , "((a b (c))), because  () is a list.")
-assert(equals( cons('a', []), ['a'] ), "(a).")
+assert(eq( cons('peanut', ['butter', 'jelly']) , [ 'peanut', 'butter', 'jelly' ]), "(peanut butter and jelly), because cons adds  an  atom to  the front of a list.")
+assert(eq( cons(['banana', 'and'], [ 'peanut', 'butter', 'jelly' ]) , [ [ 'banana', 'and' ], 'peanut', 'butter', 'jelly' ]), "((banana and) peanut butter and jelly), because cons adds any S-expression to the front of a list.")
+assert(eq( cons([['help'], 'this'], ['is', 'very', [['very'], 'to', 'learn']]) , [ [ [ 'help' ], 'this' ], 'is', 'very', [ [ 'very' ], 'to', 'learn' ] ]), "(((help) this) is  very  ((hard) to learn)).")
+assert(eq( cons(['a', 'b', ['c']], []), [ [ 'a', 'b', [ 'c' ] ] ]) , "((a b (c))), because  () is a list.")
+assert(eq( cons('a', []), ['a'] ), "(a).")
 assert.throws(() => { cons([['a', 'b', 'c']], 'b') }, "No answer, since the second argument l must be a list. Page 8")
 assert.throws(() => { cons('a', 'b') }, "No answer, since the second argument l must be a list. Page 8")
 
@@ -50,8 +51,8 @@ assert.throws(() => { cons('a', 'b') }, "No answer, since the second argument l 
 // The second argument to cons must be  a list.
 // The result is a list.
 
-assert(equals( cons('a',car([['b'],'c','d'])), ['a','b']), "['a','b']")
-assert(equals( cons('a', cdr([['b'],'c','d'])), ['a','c','d']), "['a','c','d']")
+assert(eq( cons('a',car([['b'],'c','d'])), ['a','b']), "['a','b']")
+assert(eq( cons('a', cdr([['b'],'c','d'])), ['a','c','d']), "['a','c','d']")
 
 
 assert( isNull([]) , "Yes, because it is the list composed of zero S-expressions. This question can also be written: (null? l).")
@@ -70,19 +71,23 @@ assert( isAtom(car(['Harry','had','a','heap','of','apples'])), "True, because (c
 assert( isAtom(cdr(['Harry','had','a','heap','of','apples'])) === false, "False")
 assert( isAtom(cdr(['Harry'])) === false, "False, because  the  list () is not an  atom.")
 assert( isAtom(car(cdr(['swing', 'low', 'sweet', 'cherry', 'oat']))), "True, because (cdr l) is (low sweet cherry oat), and (car (cdr l)) is low, which is an  atom.")
+assert( isAtom(car(cdr(['swing', ['low', 'sweet'], 'cherry', 'oat'])) === false), "False, since (cdr l) is ((low sweet) cherry oat), and (car (cdr l)) is (low sweet), which is a list.")
 
+assert( eq('Harry', 'Harry'), 'True, because (eq? a1  a2) is just another way to ask, "Are a1 and a2 the same non-numeric atom?"')
+assert( eq('margaring', 'butter') === false, 'False, since a1 and a2 are different atoms.')
+assert( eq.length === 2, "It takes two arguments. Both of them must be non-numeric atoms.")
+assert( eq(quote(), quote('strawberry')) === false, "No answer, () and (strawberry) are lists. In practice, lists be arguments of eq?. Two lists are eq? if they are the same list.")
+assert( eq(6, 7) === false, 'No answer, 6 and 7 are numbers.')
 
+// The Law of Eq?
+// The primitive eq? takes two ar­guments.
+// Each must be a non­numeric atom.
 
-console.log(  )
-
-
-
-
-
-function equals (a, b) {
-	if (isAtom(a)) return a == b
-	return a.every(listEq(b)) && b.every(listEq(a))
-}
-function listEq(y) {
-	return (x, n) => equals(x, y[n])
+assert( eq (car ( quote('Mary','had','a','little','lamb','chop')), 'Mary'), "True, because (car l) is the atom Mary, and the argument a is also the atom Mary.")
+assert.throws( () => { eq (cdr ( quote('soured', 'milk')), 'milk') }, "No answer. See The Laws of Eq? and Cdr.")
+{
+	const l = quote('beans', 'beans', 'we',  'need', 'jelly', 'beans')
+	assert(
+		eq (car (l), car (cdr (l)))
+		, "True, because it compares the first and second atoms in the list.")
 }
